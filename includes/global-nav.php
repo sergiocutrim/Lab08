defdcc449dbca0b834ba66a5dacaa5e4b1552e87
sergiocutrim <97315931+sergiocutrim@users.noreply.github.com>
2024-03-11 15:29:09 -0400

    Add files via upload

--- includes/global-nav.php
+++ includes/global-nav.php
@@ -2,12 +2,12 @@
 			<!-- Global site navigation -->
 			<nav>
 				<ul>
-					<li><a href="/index.php" title="Go to the Home page">Home</a></li>
-					<li><a href="/red.php" title="Learn about red blocks">Red Blocks</a></li>
-					<li><a href="/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
-					<li><a href="/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
-					<li><a href="/green.php" title="Learn about green blocks">Green Blocks</a></li>
-					<li><a href="/about.php" title="Learn more about us">About</a></li>
+					<li><a href="../Lab08/index.php" title="Go to the Home page">Home</a></li>
+					<li><a href="../Lab08/red.php" title="Learn about red blocks">Red Blocks</a></li>
+					<li><a href="../Lab08/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+					<li><a href="../Lab08/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+					<li><a href="../Lab08/green.php" title="Learn about green blocks">Green Blocks</a></li>
+					<li><a href="../Lab08/about.php" title="Learn more about us">About</a></li>
 				</ul>
 			</nav>
 <!-- STEP 4: Modify all of the above href relative paths so that they start with '/' - this means that they all point to the host name, and that the files reside in the site's public directory -->
